c6099c20fdccce6cffa5abe432de3a1504a0ecbf
paul <yummta@gmail.com>
2019-07-30 12:21:02 -0500

    Instalation and first steps

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+/** @jsx jsx */
+import { jsx } from "@emotion/core";
+import React from "react";
+import { motion } from "framer-motion";
 
 function App() {
+  const [box, setBox] = React.useState("active");
+  const variants = {
+    active: {
+      backgroundColor: "#9c27b0"
+    },
+    inactive: {
+      backgroundColor: "#e91e63"
+    }
+  };
+
+  function onChange() {
+    box === "active" ? setBox("inactive") : setBox("active");
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <motion.div
+        variants={variants}
+        animate={box}
+        css={{
+          width: 100,
+          height: 100,
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white"
+        }}
+      >
+        Hello
+      </motion.div>
+      <button onClick={onChange}>Change</button>
+    </>
   );
 }
 
